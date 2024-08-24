6f25941f44759b68f6c2f4ed73c5af9989df11e0
yusuke abe <yusuke@slidict.io>
2024-08-24 11:11:10 +0900

    First commit

---
+++ lib/coelacanth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "coelacanth/version"
+
+module Coelacanth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/coelacanth_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Coelacanth do
+  it "has a version number" do
+    expect(Coelacanth::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

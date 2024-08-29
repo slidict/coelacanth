1a424670588de07dfe5616ec5548a40735dcc34a
yusuke <1319390+yubele@users.noreply.github.com>
2024-08-29 21:43:34 +0900

    v0.1.5 (#11)
    * feat: Implemented the Coelacanth.analyze(url).
    
    * chore: Bump up v0.1.5
    
    * fix: Fixed the bugs for resolve_redirect.

--- lib/coelacanth/client.rb
+++ lib/coelacanth/client.rb
@@ -1,45 +1,75 @@
 # frozen_string_literal: true
 
 require "ferrum"
+require "oga"
 
 module Coelacanth
   # Coelacanth::Client
   class Client
-    def initialize
-      @config = Configure.new
+    def initialize(url = nil)
+      @config = Coelacanth.config
+      @url = url if url && valid_url?(url)
     end
 
-    def valid_url?(url)
-      uri = URI.parse(url)
+    def valid_url?(url = nil)
+      @url = url if url
+      uri = URI.parse(@url)
       uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
     rescue URI::InvalidURIError
       false
     end
 
-    def resolve_redirect(url, limit = 10)
+    def resolve_redirect(url = nil, limit = 10)
+      @url = url if url && valid_url?(url)
       raise Coelacanth::DeepRedirectError, "Too many redirect" if limit.zero?
-      raise Coelacanth::RedirectError, "Url or location is nil" if url.nil?
+      raise Coelacanth::RedirectError, "Url or location is nil" if @url.nil?
 
-      response = get_response(url)
+      get_response(@url)
+      handle_response(@origin_response, limit)
+    end
 
-      case response
-      when Net::HTTPSuccess then url
-      when Net::HTTPRedirection then resolve_redirect(response["location"], limit - 1)
+    def oga(url = nil)
+      @url = url if url && valid_url?(url)
+      Oga.parse_xml(get_response(@url))
+    end
+
+    def get_response(url = nil)
+      @url = url if url && valid_url?(url)
+      if @config.read("use_remote_client")
+        response_by_remote_client
+      else
+        response_by_net_http
+      end
+    end
+
+    private
+
+    def handle_response(response, limit)
+      codes = Net::HTTPResponse::CODE_CLASS_TO_OBJ.invert
+      case @status_code.to_s
+      when /^#{codes[Net::HTTPSuccess]}\d\d$/
+        @url
+      when /^#{codes[Net::HTTPRedirection]}\d\d$/
+        @url = response["location"]
+        resolve_redirect(response["location"], limit - 1)
       else
         raise Coelacanth::RedirectError
       end
     end
 
-    def get_response(url)
-      if @config.read("use_remote_client")
-        remote_client.goto(url)
-        remote_client.body
-      else
-        Net::HTTP.get_response(url)
-      end
+    def response_by_remote_client
+      remote_client.goto(@url)
+      @status_code = remote_client.network.status
+      @origin_response = remote_client
+      remote_client.body
     end
 
-    private
+    def response_by_net_http
+      response = Net::HTTP.get_response(URI.parse(@url))
+      @status_code = response.code
+      @origin_response = response
+      response.body
+    end
 
     def remote_client
       if @remote_client.nil?

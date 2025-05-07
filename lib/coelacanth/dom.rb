6b0a432f7e5ed0116218b6485a8b5333f7d2a415
yusuke <1319390+yubele@users.noreply.github.com>
2025-05-07 21:19:17 +0900

    fix: Replace Ferrum with Net::HTTP due to errors with ScreenshotOne

--- lib/coelacanth/dom.rb
+++ lib/coelacanth/dom.rb
@@ -6,7 +6,7 @@ module Coelacanth
   # Coelacanth::Dom
   class Dom
     def oga(url)
-      Oga.parse_xml(Client::Ferrum.new(url).get_response)
+      Oga.parse_xml(Net::HTTP.get_response(URI.parse(url)))
     end
   end
 end

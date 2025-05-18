677d151df6f7616b85c268d7ff3590a0cc70781c
yusuke abe <yusuke@slidict.io>
2025-05-18 17:18:08 +0900

    fix: support ferrum 0.17

--- lib/coelacanth/client/ferrum.rb
+++ lib/coelacanth/client/ferrum.rb
@@ -28,14 +28,14 @@ module Coelacanth::Client
     private
 
     def remote_client
-      if @remote_client.nil?
-        headers = @config.read("remote_client.headers")
-        @remote_client = ::Ferrum::Browser.new(
-          ws_url: @config.read("remote_client.ws_url"),
-          timeout: @config.read("remote_client.timeout")
-        ).create_page
-        @remote_client.headers.set(headers) unless headers.empty?
-      end
+      return @remote_client if @remote_client
+
+      headers = @config.read("remote_client.headers")
+      @remote_client = @browser = ::Ferrum::Browser.new(
+        ws_url: @config.read("remote_client.ws_url"),
+        timeout: @config.read("remote_client.timeout")
+      )
+      @browser.headers.set(headers) unless headers.empty?
       @remote_client
     end
   end
